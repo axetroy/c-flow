1902e9e4c6ee87b4aa3f53379bd5c8ef8a49fe00
axetroy <troy450409405@gmail.com>
2017-04-06 14:35:41 +0800

    Updates

---
+++ flow.js
@@ -0,0 +1,156 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["Flow"] = factory();
+	else
+		root["Flow"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/**
+ * Created by axetroy on 2017/4/4.
+ */
+
+var Flow = function () {
+  function Flow(concurrent) {
+    _classCallCheck(this, Flow);
+
+    this.__concurrent = concurrent;
+    this.__tasks = [];
+    this.__tasksInQueue = [];
+    this.__tasksHaveDoned = 0;
+  }
+
+  _createClass(Flow, [{
+    key: "append",
+    value: function append(fn) {
+      this.__tasks.push(fn);
+      return this;
+    }
+  }, {
+    key: "_next",
+    value: function _next(lastTask) {
+      this.__tasksHaveDoned++;
+      if (this.__tasksHaveDoned === this.__tasks.length) {
+        this.__tasksHaveDoned = 0;
+        this.__tasksInQueue = [];
+        this.done();
+      }
+
+      var currentTask = this.__tasksInQueue.shift();
+
+      currentTask && currentTask.call(this, this._next.bind(this, currentTask));
+
+      return this;
+    }
+  }, {
+    key: "run",
+    value: function run() {
+      var _this = this;
+
+      // init
+      this.__tasksInQueue = this.__tasks.slice();
+      this.__tasksHaveDoned = 0;
+
+      var tasks = [];
+
+      for (var i = 0; i < this.__concurrent; i++) {
+        tasks.push(this.__tasksInQueue.shift());
+      }
+
+      // start run task
+      tasks.forEach(function (task) {
+        return task.call(_this, _this._next.bind(_this, task));
+      });
+
+      return new Promise(function (resolve, reject) {
+        return _this.done = resolve;
+      });
+    }
+  }]);
+
+  return Flow;
+}();
+
+module.exports = Flow;
+
+/***/ })
+/******/ ]);
+});
